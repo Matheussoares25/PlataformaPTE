933d8f5beeee01e3115f6df02806572dd2131d15
ti2 <ti2@NB-TI2>
2026-07-13 11:53:41 -0300

    implementação dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Navbar from "./components/Navbar";
 import TreinamentoSidebar from "./components/CriaTComponentes/TreinamentoSideBar";
 import PrivateRoute from "./services/PrivateRoute";
 import LoadingOverlay from "./components/LoadingOverlay";
+import Dashboard from "./pages/Dashboard";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "./App.css";
@@ -63,6 +64,15 @@ function App() {
             <PrivateRoute>
               <Navbar />
               <TreinamentoCreate />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/Dashboard"
+          element={
+            <PrivateRoute>
+              <Navbar />
+              <Dashboard />
             </PrivateRoute>
           }
         />
---
+++ src/components/DashBoardComponentes/CursosCriados.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import Api from "../../services/EndPoint";
+
+export default function CursosCriados() {
+  const [cursosCriados, setCursosCriados] = useState([]);
+  const [carregando, setCarregando] = useState(true);
+  const [erro, setErro] = useState(null);
+
+  useEffect(() => {
+    const buscarDados = async () => {
+      try {
+        const criados = await Api.CallEndpoint("Treinamento", "GET");
+
+        setCursosCriados(criados.length);
+      } catch (err) {
+      console.error(err);
+            setErro("Não foi possível carregar os dados de cursos.");
+      }
+    
+      finally {
+        setCarregando(false);
+      }
+    };
+
+    buscarDados();
+  }, []);
+
+  return (
+    <div className="kpi-card kpi-red">
+      <div className="kpi-icon">
+        <i className="fa-solid fa-book"></i>
+      </div>
+      <div>
+        <p className="kpi-label">Cursos Criados</p>
+        {carregando ? (
+          <p className="kpi-value">Carregando...</p>
+        ) : erro ? (
+          <h6 className="kpi-value">{erro}</h6>
+        ) : (
+          <p className="kpi-value">{cursosCriados}</p>
+        )}
+      </div>
+    </div>
+  );
+}
